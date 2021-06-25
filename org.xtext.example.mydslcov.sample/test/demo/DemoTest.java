ca8e09b229a25089a67a74d22c92cf3fa1e8665e
Christian Dietrich <christian.dietrich@itemis.de>
2021-06-25 09:12:07 +0200

    inital checkin
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ org.xtext.example.mydslcov.sample/test/demo/DemoTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.junit.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testIt() {
+		Greeter_sample.main(new String[] {});
+		System.out.println(Dully.class);
+	}
+
+}
